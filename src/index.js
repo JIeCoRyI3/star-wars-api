27bab89b120f783659102f6240dc03cdd9261131
JIeCoRyI3 <jiecoryi3@gmail.com>
2020-03-18 15:06:47 +0200

    Show random planet every reload

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import SwapiService from "./services/swapi-service";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
-
-const connect = new SwapiService();
-connect.getAllStarships().then((res) => console.log(res));
 
 ReactDOM.render(<App/>, document.getElementById('root'));
